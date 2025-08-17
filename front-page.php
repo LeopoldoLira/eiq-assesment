4bc07d82351255c6e43738c879c08f2877bf4320
Leopoldo <lira.leopoldo97@gmail.com>
2025-08-16 22:12:12 -0600

    task(front-page.php): added the hero section details some animations.

--- front-page.php
+++ front-page.php
@@ -11,4 +11,45 @@
 <? get_header();?>
 
 
+<section class='hero-section' style='background-image: url("/wp-content/themes/elementiq/assets/images/herobackground.png");'>
+    <h1>A 50-Year Legacy of Building Community Based Housing</h1>
+
+    <div class='hero-section__cards'>
+      <div class='hero-section__cards--card'>
+        <div><h2>RENTALS</h2></div>
+        <div class='divider'>
+          <div class='divider-line'></div>
+          <img src='/wp-content/themes/elementiq/assets/images/grey-star.svg' />
+          <div class='divider-line'></div>
+        </div>
+        <div><p>Molnar is committed to quality rentals for Vancouver families in need.</p></div>
+      </div>
+      <div class='hero-section__cards--card'>
+        <div><h2>CONDOS</h2></div>
+        <div class='divider'>
+          <div class='divider-line'></div>
+          <img src='/wp-content/themes/elementiq/assets/images/grey-star.svg' />
+          <div class='divider-line'></div>
+        </div>
+        <div><p>Molnar condos blend style and function—award-winning homes for families.</p></div>
+      </div>
+      <div class='hero-section__cards--card'>
+        <div><h2>COMMERCIAL</h2></div>
+        <div class='divider'>
+          <div class='divider-line'></div>
+          <img src='/wp-content/themes/elementiq/assets/images/grey-star.svg' />
+          <div class='divider-line'></div>
+        </div>
+        <div><p>We revitalize commercial spaces to meet real community needs.</p></div>
+      </div>
+    </div>
+</section>
+
+
+
+<section style='padding: 100rem 10rem'>
+    <p>Filler text for testing the shape divider</p>
+</section>
+
+
 <? get_footer();?>
